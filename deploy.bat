01214bf606bc232ecfada2bddb2211b886a2d5fa
baylarvaliyev <baylarv@gmail.com>
2026-06-11 12:14:13 +0400

    Fix2-mobile-menu

--- deploy.bat
+++ deploy.bat
@@ -2,14 +2,9 @@
 set GIT=C:\Users\Baylar\AppData\Local\GitHubDesktop\app-3.5.12\resources\app\git\mingw64\bin\git.exe
 set REPO=C:\Users\Baylar\Documents\GitHub\adsouns
 
-echo === Adding all files ===
 %GIT% -C %REPO% add -A
-
-echo === Committing ===
-%GIT% -C %REPO% commit -m "Fix: Lenis removed, English blogs x9, blog index EN, touchpad lag fixed"
-
-echo === Pushing ===
+%GIT% -C %REPO% status --short
+%GIT% -C %REPO% commit -m "%1"
 %GIT% -C %REPO% push
-
-echo === Done ===
-%GIT% -C %REPO% log --oneline -3
+%GIT% -C %REPO% log --oneline -2
+echo DONE
